f2559dd2de832f536603b87151d39396535bd759
vietviet08 <vietnq.23ceb@vku.udn.vn>
2026-04-17 14:18:15 +0700

    Add Windows runner files and integrate geolocator plugin
    - Created generated_plugin_registrant.cc and generated_plugin_registrant.h for plugin registration.
    - Added generated_plugins.cmake to manage plugin dependencies.
    - Implemented CMakeLists.txt for the Windows runner with necessary configurations.
    - Created Runner.rc for application resources including icon and version info.
    - Developed flutter_window.cpp and flutter_window.h to manage the Flutter window.
    - Implemented main.cpp for application entry point and command line argument handling.
    - Added resource.h for resource definitions.
    - Included app_icon.ico as the application icon.
    - Created runner.exe.manifest for application settings.
    - Developed utils.cpp and utils.h for console management and command line argument parsing.
    - Implemented win32_window.cpp and win32_window.h for high DPI-aware window management.

---
+++ lab8_bmi_calculator/android/app/src/main/kotlin/com/example/lab8_bmi_calculator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lab8_bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lab9_clima/android/app/src/main/kotlin/com/example/lab9_clima/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lab9_clima
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
